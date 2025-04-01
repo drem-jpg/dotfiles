cee0d7fb98679c1862e1b8ff300b634e0cfe85f9
drem-jpg <drem687sawesomeemail@gmail.com>
2025-04-01 17:51:26 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu btop cava dunst fastfetch hypridle hyprlock jack2 kitty ly neofetch neovim pipewire pipewire-audio pipewire-pulse  thunar waybar wireplumber yazi zsh
+sudo pacman -Syu btop cava dunst fastfetch hypridle hyprlock jack2 kitty ly neofetch neovim pipewire pipewire-audio pipewire-pulse thunar waybar wireplumber yazi zsh
 paru -S tofi
 systemctl enable ly.service
 stow .local alacritty backgrounds btop cava dunst fastfetch hypr kitty neofetch nvim p10k tofi waybar yazi zshrc
