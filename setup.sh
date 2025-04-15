3ee38811bbeb73be4ab86f22cedcd2176577c03e
drem-jpg <drem687sawesomeemail@gmail.com>
2025-04-15 19:21:14 -0400

    remove setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-sudo pacman -Syu btop cava dunst fastfetch hypridle hyprlock jack2 kitty ly neofetch neovim nwg-look pipewire pipewire-audio pipewire-pulse thunar waybar wireplumber yazi zsh
-paru -S catppuccin-gtk-theme-mocha-revamped tofi
-sudo systemctl enable ly.service
-stow .local alacritty backgrounds btop cava dunst fastfetch hypr kitty neofetch nvim p10k tofi waybar yazi zshrc
-git clone https://github.com/vinceliuice/Tela-circle-icon-theme.git
-cd Tela-circle-icon-theme
-./install.sh -a
